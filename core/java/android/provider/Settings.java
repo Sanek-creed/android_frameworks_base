ade2864527b9cfef3842d170d322d09fd47fb868
calebcabob <calphonic@gmail.com>
2019-03-26 20:24:30 -0400

    NetworkTraffic: Allow user to choose positioning [1/2]
    SDB - Bring it for PiA Devices aka Notch Devices
    ** Cleanup Codes to make it easy for noobs like me
    
    This allows the user to choose whether or not they want to see the traffic indicator in status bar or in the expanded header
    
    Change-Id: Ifff10bcccde91ea90f7bfe85acfe70ffc54a9418

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5284,6 +5284,14 @@ public final class Settings {
          public static final String NETWORK_TRAFFIC_FONT_SIZE = "network_traffic_font_size";
 
         /**
+         * Show network traffic indicator
+         * 0 - Statusbar (default)
+         * 1 - QS Header
+         * @hide
+         */
+         public static final String NETWORK_TRAFFIC_VIEW_LOCATION = "network_traffic_view_location";
+
+        /**
          * These are all public system settings
          *
          * @hide
@@ -5396,6 +5404,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(VIBRATE_ON_CALLWAITING);
             PRIVATE_SETTINGS.add(VIBRATE_ON_DISCONNECT);
             PRIVATE_SETTINGS.add(COLOR_BUCKET_OVERLAY);
+            PRIVATE_SETTINGS.add(NETWORK_TRAFFIC_VIEW_LOCATION);
         }
 
         /**
