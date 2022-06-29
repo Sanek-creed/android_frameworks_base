db9e88929418a49d192ffafdab214690e1bc7419
Hyper <hyper@remainsilent.net>
2022-06-29 16:02:08 +0530

    Revert "fixup! SystemUI: cleanup smartspace"
    This reverts commit 602433bc3c3b28282f79eb90fcb83b38ccd42549.

--- packages/SystemUI/src/com/android/systemui/custom/CustomSystemUIFactory.java
+++ packages/SystemUI/src/com/android/systemui/custom/CustomSystemUIFactory.java
@@ -5,14 +5,10 @@ import android.content.res.AssetManager;
 
 import com.android.systemui.custom.dagger.DaggerCustomGlobalRootComponent;
 import com.android.systemui.custom.dagger.CustomGlobalRootComponent;
-import com.android.systemui.custom.dagger.CustomSysUIComponent;
 import com.android.systemui.dagger.GlobalRootComponent;
 import com.android.systemui.navigationbar.gestural.BackGestureTfClassifierProvider;
 import com.android.systemui.SystemUIFactory;
-import com.android.systemui.screenshot.ScreenshotNotificationSmartActionsProvider;
 import com.google.android.systemui.gesture.BackGestureTfClassifierProviderGoogle;
-
-import java.util.concurrent.ExecutionException;
 
 public class CustomSystemUIFactory extends SystemUIFactory {
     @Override
@@ -25,13 +21,5 @@ public class CustomSystemUIFactory extends SystemUIFactory {
     @Override
     public BackGestureTfClassifierProvider createBackGestureTfClassifierProvider(AssetManager am, String modelName) {
         return new BackGestureTfClassifierProviderGoogle(am, modelName);
-    }
-
-    @Override
-    public void init(Context context, boolean fromTest) throws ExecutionException, InterruptedException {
-        super.init(context, fromTest);
-        if (shouldInitializeComponents()) {
-            ((CustomSysUIComponent) getSysUIComponent()).createKeyguardSmartspaceController();
-        }
     }
 }
